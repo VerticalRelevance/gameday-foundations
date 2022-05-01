d0138ab9ada3fc42828d8397049dcb6e622eea05
sachinmahashabde <sachin.mahashabde@student.shu.edu>
2022-05-01 18:22:17 -0400

    added in iam policy

--- pipeline_cdk/pipeline_cdk/pipeline_cdk_stack.py
+++ pipeline_cdk/pipeline_cdk/pipeline_cdk_stack.py
@@ -6,15 +6,53 @@ from aws_cdk import (
     aws_codestarconnections as codestar,
     aws_codebuild as codebuild,
     aws_codepipeline_actions as codepipeline_actions,
+    aws_iam as iam
 )
 from constructs import Construct
 
 class PipelineCdkStack(Stack):
 
+
+    def createCodePipelinePolicy(self, codestar_connections_github_arn):
+        codepipeline_policy= iam.ManagedPolicy(
+            self, "codepipeline_policy",
+            managed_policy_name="codepipeline_policy_gameday",
+            statements=[
+                iam.PolicyStatement(
+                    effect = iam.Effect.ALLOW,
+                    actions= [
+                        "ssm:CreateDocument"
+                    ],
+
+                    resources=[
+                        "document"
+                    ]
+                ),
+
+                iam.PolicyStatement(
+                    effect = iam.Effect.ALLOW,
+                    actions= [
+                        "codestar-connections:UseConnection"
+                    ],
+                    resources=[
+                        codestar_connections_github_arn
+                    ],
+                ),
+
+                iam.PolicyStatement(
+                    effect = iam.Effect.Allow,
+                    actions = [
+                        "codebuild:StartBuild"
+                    ],
+
+                )
+            ]
+        )
+
     def __init__(self, scope: Construct, construct_id: str, **kwargs):
         super().__init__(scope, construct_id, **kwargs)
-
         codestar_connections_github_arn = "arn:aws:codestar-connections:region:account-id:connection/connection-id"
+
         source_output = codepipeline.Artifact()
         source_action = codepipeline_actions.CodeStarConnectionsSourceAction(
                 action_name="Github",
@@ -40,13 +78,7 @@ class PipelineCdkStack(Stack):
                     ],
                 )
                
-            ]
-            
-            
-
-             
-            
-
+            ]                                         
         )
             
 
