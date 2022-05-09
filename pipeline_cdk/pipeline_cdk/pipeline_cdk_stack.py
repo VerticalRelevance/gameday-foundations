de371353002313b414efd43270243c01ea5c431c
sachinmahashabde <sachin.mahashabde@student.shu.edu>
2022-05-09 12:58:14 -0400

    trying another buildspec change

--- pipeline_cdk/pipeline_cdk/pipeline_cdk_stack.py
+++ pipeline_cdk/pipeline_cdk/pipeline_cdk_stack.py
@@ -81,17 +81,7 @@ class GameDayPipelineStack(Stack):
                                       ),
 
 
-                                      commands= ["npm install -g aws-cdk",
-                                            "cd runbooks",
-                                            "echo 'Entered Runbooks directory'",
-
-                                            """for file in "/runbooks"/*; do
-                                                echo "uploading $file"
-                                                docname = basename $file .yml
-                                                aws ssm create-document \
-                                                    --content file://$file
-                                                    --name $docname
-                                                    --document-type "Automation""".replace('\n', ' ')]
+                                      commands= ["./upload.sh"]
                                         
                                       )
         )
