0b070717b3f6e875f9a733b87fc6d770176d1bbd
sachinmahashabde <sachin.mahashabde@student.shu.edu>
2022-05-09 14:04:21 -0400

    errors in resources iam policy

--- pipeline_cdk/pipeline_cdk/pipeline_cdk_stack.py
+++ pipeline_cdk/pipeline_cdk/pipeline_cdk_stack.py
@@ -94,7 +94,7 @@ class GameDayPipelineStack(Stack):
                         ],
 
                         resources=[
-                            "document"
+                            "*"
                         ]
                     )
                 ]    
